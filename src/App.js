38d7ced9add5d8729659524a0ca4c78b7172f5e8
Hadar Rose <hadarose@gmail.com>
2021-06-21 09:32:14 +0300

    reverted back to last commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import CalculatorGrid from "./components/CalculatorGrid";
+import Calculator from "./components/Calculator";
 
 function App() {
   return (
     <div>
       <h1 className="mt-2">Calc App</h1>
       <p>Do all the math you want</p>
-      <CalculatorGrid />
+      <Calculator />
     </div>
   );
 }
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,87 +2,68 @@ import Display from "./Display";
 import { useState } from "react";
 
 const Calculator = () => {
-  const [displayNumber, setDisplayNumber] = useState("0");
+  const [displayValue, setDisplayValue] = useState("0");
   const [value, setValue] = useState(null);
   const [operator, setOperator] = useState(null);
   const [isPendingOperand, setIsPendingOperand] = useState(false);
   const [lastOperand, setLastOperand] = useState(null);
 
   const clearContent = () => {
-    setDisplayNumber("0");
+    setDisplayValue("0");
     setValue(null);
     setOperator(null);
     setIsPendingOperand(false);
   };
 
   const handleKey = (key) => {
-    console.log("what key was pressed? ", key, typeof key);
-    if (isNaN(key)) {
-      if (
-        key === "=" ||
-        key === "Enter" ||
-        key === "/" ||
-        key === "*" ||
-        key === "-" ||
-        key === "+"
-      ) {
-        handleOperator(key);
-        return;
-      } else if (key === "Backspace") {
-        clearContent();
-        return;
-      } else {
-        return;
-      }
-    }
-
-    setLastOperand(key);
+    setLastOperand(Number(key));
 
     if (isPendingOperand) {
-      setDisplayNumber(key);
+      setDisplayValue(key);
       setIsPendingOperand(false);
     } else {
-      setDisplayNumber(displayNumber === "0" ? key : displayNumber + key);
+      setDisplayValue(displayValue === "0" ? key : displayValue + key);
     }
   };
 
   const handleOperator = (inputOperator) => {
-    const operand = Number(displayNumber);
-    console.log("what is operand? ", operand);
-    console.log("wat is lastoperand? ", lastOperand);
-
-    setOperator(inputOperator);
-    setIsPendingOperand(true);
+    const operand = Number(displayValue);
 
     if (!value) {
       setValue(operand);
-    } else if (operator) {
+    } else if (operator && !isPendingOperand) {
       setValue(calculate(value, operand));
-      setDisplayNumber(String(calculate(value, operand)));
+      setDisplayValue(String(calculate(value, operand)));
+    } else if (operator && isPendingOperand && inputOperator === "=") {
+      setValue(calculate(value, value));
+      setDisplayValue(String(calculate(value, value)));
     }
+
+    setOperator(inputOperator);
+    setIsPendingOperand(true);
   };
 
-  const calculate = (previousValue, currentValue) => {
-    console.log(previousValue, currentValue, typeof currentValue);
+  const calculate = (previousValue, operand) => {
     let result = previousValue;
-
     switch (operator) {
       case "+":
-        result += currentValue;
+        result += operand;
         break;
       case "-":
-        result -= currentValue;
+        result -= operand;
         break;
       case "/":
-        result /= currentValue;
+        result /= operand;
         break;
       case "*":
-        result *= currentValue;
+        result *= operand;
         break;
       case "=":
-      case "Enter":
-
-        result = currentValue + lastOperand;
+        if (lastOperand) {
+          result = operand + lastOperand;
+        } else {
+          result = operand;
+        }
         break;
       default:
         break;
@@ -92,135 +73,130 @@ const Calculator = () => {
   };
 
   return (
-    <div
-      className="container vh-100 vw-100"
-      style={{ outline: 0 }}
-      tabIndex="1"
-      onKeyDown={(event) => handleKey(event.key)}
-    >
-      what is value? {value} <br />
-      what is operator? {operator} <br />
-      what is isPendingOperand? {isPendingOperand && "true"} <br />
-      <div className="row d-flex justify-content-center">
-        <div className="col-4 py-3 border border-dark d-flex justify-content-end bg-dark text-light">
-          <Display display={displayNumber} />
+    <div>
+      <div className="container">
+        <div className="row d-flex justify-content-center">
+          <div className="col-4 py-3 border border-dark d-flex justify-content-end bg-dark text-light">
+            <Display display={displayValue} />
+          </div>
         </div>
-      </div>
-      <div className="row d-flex justify-content-center">
-        <button
-          onClick={clearContent}
-          className="col-1 py-2 border border-dark bg-light text-dark"
-        >
-          AC
-        </button>
-        <div className="col-1 py-2 border border-dark bg-light text-dark" />
-        <div className="col-1 py-2 border border-dark bg-light text-dark" />
-        <button
-          className="col-1 py-2 btn border border-dark btn-warning rounded-0"
-          data-toggle="button"
-          aria-pressed="false"
-          onClick={() => handleOperator("/")}
-        >
-          &#247;
-        </button>
-      </div>
-      <div className="row d-flex justify-content-center">
-        <button
-          onClick={() => handleKey("7")}
-          className="col-1 py-2 border border-dark bg-secondary text-white"
-        >
-          7
-        </button>
-        <button
-          onClick={() => handleKey("8")}
-          className="col-1 py-2 border border-dark bg-secondary text-white"
-        >
-          8
-        </button>
-        <button
-          onClick={() => handleKey("9")}
-          className="col-1 py-2 border border-dark bg-secondary text-white"
-        >
-          9
-        </button>
-        <button
-          className="col-1 py-2 btn border border-dark btn-warning rounded-0"
-          data-toggle="button"
-          aria-pressed="false"
-          onClick={() => handleOperator("*")}
-        >
-          &#215;
-        </button>
-      </div>
-      <div className="row d-flex justify-content-center">
-        <button
-          onClick={() => handleKey("4")}
-          className="col-1 py-2 border border-dark bg-secondary text-white"
-        >
-          4
-        </button>
-        <button
-          onClick={() => handleKey("5")}
-          className="col-1 py-2 border border-dark bg-secondary text-white"
-        >
-          5
-        </button>
-        <button
-          onClick={() => handleKey("6")}
-          className="col-1 py-2 border border-dark bg-secondary text-white"
-        >
-          6
-        </button>
-        <button
-          className="col-1 py-2 btn border border-dark btn-warning rounded-0"
-          data-toggle="button"
-          aria-pressed="false"
-          onClick={() => handleOperator("-")}
-        >
-          -
-        </button>
-      </div>
-      <div className="row d-flex justify-content-center">
-        <button
-          onClick={() => handleKey("1")}
-          className="col-1 py-2 border border-dark bg-secondary text-white"
-        >
-          1
-        </button>
-        <button
-          onClick={() => handleKey("2")}
-          className="col-1 py-2 border border-dark bg-secondary text-white"
-        >
-          2
-        </button>
-        <button
-          onClick={() => handleKey("3")}
-          className="col-1 py-2 border border-dark bg-secondary text-white"
-        >
-          3
-        </button>
-        <button
-          className="col-1 py-2 btn border border-dark btn-warning rounded-0"
-          data-toggle="button"
-          aria-pressed="false"
-          onClick={() => handleOperator("+")}
-        >
-          +
-        </button>
-      </div>
-      <div className="row d-flex justify-content-center">
-        <button
-          className="col-3 py-2 border border-dark bg-secondary text-white"
-          onClick={() => handleKey("0")}
-        >
-          0
-        </button>
-        <button
-          className="col-1 py-2 border border-dark bg-warning"
-          onClick={() => handleOperator("=")}
-        >
-          =
-        </button>
+
+        <div className="row d-flex justify-content-center">
+          <button
+            onClick={clearContent}
+            className="col-1 py-2 border border-dark bg-light text-dark"
+          >
+            AC
+          </button>
+          <div className="col-1 py-2 border border-dark bg-light text-dark" />
+          <div className="col-1 py-2 border border-dark bg-light text-dark" />
+          <button
+            className="col-1 py-2 btn border border-dark btn-warning rounded-0"
+            data-toggle="button"
+            aria-pressed="false"
+            onClick={() => handleOperator("/")}
+          >
+            &#247;
+          </button>
+        </div>
+        <div className="row d-flex justify-content-center">
+          <button
+            onClick={() => handleKey("7")}
+            className="col-1 py-2 border border-dark bg-secondary text-white"
+          >
+            7
+          </button>
+          <button
+            onClick={() => handleKey("8")}
+            className="col-1 py-2 border border-dark bg-secondary text-white"
+          >
+            8
+          </button>
+          <button
+            onClick={() => handleKey("9")}
+            className="col-1 py-2 border border-dark bg-secondary text-white"
+          >
+            9
+          </button>
+          <button
+            className="col-1 py-2 btn border border-dark btn-warning rounded-0"
+            data-toggle="button"
+            aria-pressed="false"
+            onClick={() => handleOperator("*")}
+          >
+            &#215;
+          </button>
+        </div>
+        <div className="row d-flex justify-content-center">
+          <button
+            onClick={() => handleKey("4")}
+            className="col-1 py-2 border border-dark bg-secondary text-white"
+          >
+            4
+          </button>
+          <button
+            onClick={() => handleKey("5")}
+            className="col-1 py-2 border border-dark bg-secondary text-white"
+          >
+            5
+          </button>
+          <button
+            onClick={() => handleKey("6")}
+            className="col-1 py-2 border border-dark bg-secondary text-white"
+          >
+            6
+          </button>
+          <button
+            className="col-1 py-2 btn border border-dark btn-warning rounded-0"
+            data-toggle="button"
+            aria-pressed="false"
+            onClick={() => handleOperator("-")}
+          >
+            -
+          </button>
+        </div>
+        <div className="row d-flex justify-content-center">
+          <button
+            onClick={() => handleKey("1")}
+            className="col-1 py-2 border border-dark bg-secondary text-white"
+          >
+            1
+          </button>
+          <button
+            onClick={() => handleKey("2")}
+            className="col-1 py-2 border border-dark bg-secondary text-white"
+          >
+            2
+          </button>
+          <button
+            onClick={() => handleKey("3")}
+            className="col-1 py-2 border border-dark bg-secondary text-white"
+          >
+            3
+          </button>
+          <button
+            className="col-1 py-2 btn border border-dark btn-warning rounded-0"
+            data-toggle="button"
+            aria-pressed="false"
+            onClick={() => handleOperator("+")}
+          >
+            +
+          </button>
+        </div>
+        <div className="row d-flex justify-content-center">
+          <button
+            className="col-3 py-2 border border-dark bg-secondary text-white"
+            onClick={() => handleKey("0")}
+          >
+            0
+          </button>
+          <button
+            className="col-1 py-2 border border-dark bg-warning"
+            onClick={() => handleOperator("=")}
+          >
+            =
+          </button>
+        </div>
       </div>
     </div>
   );
